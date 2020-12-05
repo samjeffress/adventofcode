99e70b52136fc44977e311d940d398dec0132eec
samjeffress <samjeffress@gmail.com>
2020-12-05 21:37:40 +1100

    day 5 part 2

--- 2020/haskell/src/D05.hs
+++ 2020/haskell/src/D05.hs
@@ -1,4 +1,5 @@
 module D05 where
+import Debug.Trace
 import System.IO  
 import Data.List
 
@@ -6,10 +7,10 @@ valueOfChar :: Int -> Char -> Int
 valueOfChar base char =
   if char == 'B' || char == 'R' then base else 0
 
-rowValue :: String -> (Int, String)
+rowValue :: String -> Int
 rowValue rowData = 
   case rowData of
-    (a:b:c:d:e:f:g:h:i:j) -> (8 * sum [
+    (a:b:c:d:e:f:g:h:i:j) -> 8 * sum [
         valueOfChar 64 a,
         valueOfChar 32 b,
         valueOfChar 16 c,
@@ -21,12 +22,27 @@ rowValue rowData =
         valueOfChar 4 h,
         valueOfChar 2 i,
         valueOfChar 1 (last j)
-      ], rowData)
-    _ -> (0, rowData) 
+      ]
+    _ -> 0
 
+findGap :: [Int] -> Int
+findGap sortedCodes = 
+  case sortedCodes of
+    (a:b) ->  if abs (a - head b) == 2 then a+1 else findGap (tail sortedCodes)
+    _ -> 0
+    
 one = do 
   handle <- openFile "src/05input.txt" ReadMode  
   contents <- hGetContents handle
   let l = lines contents
   let values = sort (map rowValue l)
   print (maximum values)
+  
+
+two = do 
+  handle <- openFile "src/05input.txt" ReadMode  
+  contents <- hGetContents handle
+  let l = lines contents
+  let values = sort (map rowValue l)
+  print values
+  print (findGap values)
